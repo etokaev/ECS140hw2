1f777d1a376caf89249fe63f79548d0d9617dd25
chrisjmorgan <chrisjamesmorgn@gmail.com>
2020-02-26 12:28:37 -0800

    new

--- nada/visitors/codeGeneration.java
+++ nada/visitors/codeGeneration.java
@@ -5,6 +5,27 @@ import java.util.*;
 
 class codeGenerator extends DepthFirstAdaptor
 {
+  public void inASubprogramBody(ASubprogramBody node)
+    {
+        System.out.print("public class ");
+
+    }
+
+  public void inASubprogramSpec(ASubprogramSpec node)
+  {
+    System.out.print(node.getIdent());
+    System.out.println("{");
+  }
+  public void outASubprogramSpec(ASubprogramSpec node)
+  {
+    System.out.println("}");
+  }
+
+  public
+
+
 }
+
+
 
 
