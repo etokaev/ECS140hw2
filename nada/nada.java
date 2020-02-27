b5d047a13e2a62ce97134cfd637b91cdfad5a9f0
etokaev <56102506+etokaev@users.noreply.github.com>
2020-02-26 22:48:47 -0800

    added file output and redirection

--- nada/nada.java
+++ nada/nada.java
@@ -8,12 +8,34 @@ import nada.analysis.*;
 import nada.visitors.*;
 import java.io.*;
 import java.util.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.io.PrintStream;
+import java.lang.*;
+
 
 public class nada {
 	public static void main(String[] arguments) {
 		try {
 
-               		
+			PrintStream originalOut = System.out;
+        
+			Scanner input = new Scanner(System.in);
+			//System.out.print("Enter the desired name of your file: ");
+			String fileName = args[0].substring(0,args[0].indexOf("."));
+			fileName = fileName + ".java";
+		
+			PrintStream fileOut = new PrintStream(fileName);
+			System.setOut(fileOut);					
 
            if (arguments.length == 2 && arguments[1].charAt(0) == 'D' ) {new Parser(new Lexer
            (new PushbackReader(new BufferedReader(new FileReader(arguments[0])), 1024))).parse().apply(new DebugAdapter());} 
@@ -29,6 +51,9 @@ public class nada {
 		} catch (Exception e) {
 			System.out.println("\nGENERAL ERROR: " + e.getMessage() + "\n\n");
 			e.printStackTrace(System.out);
+		} catch (IOException e){
+			System.out.println("an error occured.");
+			e.printStackTrace();
 		}
 	}
 }
